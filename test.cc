d3a918a98c3ffc24f0af1f2332f6e9a923b3c392
RyanardoDaVinci <ryansleeuwaegen@gmail.com>
2021-09-16 14:39:57 +0200

    checkt jaar en maand

--- test.cc
+++ test.cc
@@ -1,8 +1,40 @@
-   #include <iostream>
-   using namespace std;
+#include <iostream>
+#include <ctime>
+using namespace std;
 
-   int main ( ) {
-     cout << "Dag allemaal!" << endl;
-     cout << "Dag allemaal!" << endl;
-     return 0;
-   }//main
+int main ( ) {
+    int jaar;
+    int maand;
+    int dag;
+    tm s;
+    time_t t;
+
+    time (&t);
+    s = * localtime (&t);
+    int cdag = s.tm_mday;
+    int cmaand = s.tm_mon + 1; // 0 voor januari
+    int cjaar = s.tm_year + 1900; // vanaf 1900
+
+    cout <<"In welk jaar ben je geboren: ";
+    cin >> jaar;
+
+    if (jaar < cjaar - 10)
+        cout << "Sorry je bent te jong.";
+
+    else
+      if (jaar > cjaar + 100)
+      cout << "Sorry je bent te oud.";
+
+      else
+        cout <<"In welke maand ben je geboren: ";
+        cin >> maand;
+
+        if (jaar == cjaar - 10 && maand < cmaand)
+        cout << "Sorry je bent net te jong.";
+
+        else
+          if (jaar == cjaar + 100 && maand > cmaand)
+          cout << "Sorry je bent net te oud.";
+
+return 0;
+}//main end
