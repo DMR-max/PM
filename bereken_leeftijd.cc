d423ca63e04f65b26a9d618ee290e8a501fe12e3
DMR-max <60442394+DMR-max@users.noreply.github.com>
2021-09-16 15:55:14 +0200

    maandig code

--- bereken_leeftijd.cc
+++ bereken_leeftijd.cc
@@ -18,56 +18,29 @@ int main ( ) {
     int cdag = s.tm_mday;
     int cmaand = s.tm_mon + 1; // 0 voor januari
     int cjaar = s.tm_year + 1900; // vanaf 1900
-
-    cout <<"In welk jaar ben je geboren: ";
     cin >> jaar;
-
-    if (jaar < cjaar - 10){
-        cout << "Sorry je bent te jong.";
-}else{
-      if (jaar > cjaar + 100){
-      cout << "Sorry je bent te oud.";
-    }else{
-        cout <<"In welke maand ben je geboren: ";
-        cin >> maand;
-      }
-        if (jaar == cjaar - 10 && maand < cmaand){
-        cout << "Sorry je bent net te jong.";
-      } else{
-          if (jaar == cjaar + 100 && maand > cmaand){
-          cout << "Sorry je bent net te oud.";
-        }else{
-        cout <<"Op welke dag ben je geboren: ";
-        cin >> dag;
-      }
-        if (jaar == cjaar - 10 && maand == cmaand && dag < cdag){
-        cout << "Sorry je bent echt net te jong.";
-      }else{
-          if  (jaar == cjaar + 100 && maand == cmaand && dag > cdag - 1){
-          cout << "Sorry je bent echt net te oud.";
-        }
-
-          else{
-          cout << "Je bent zo oud:";
-        }
-        }
-      }
-
+    cin >> maand;
   cin >> dag;
   leeftijd_jaar = cjaar - jaar;
   leeftijd_maand = cmaand - maand;
-  if (leeftijd_maand >= 0){
+  if (leeftijd_maand == 0){
   leeftijd_dag = cdag - dag;
   if (leeftijd_dag >= 0){
-    leeftijd_jaar = leeftijd;
+    cout << leeftijd_jaar << endl;
+    cout << "gefeliciteerd je bent maandig" << endl;
   }else{
-    leeftijd_jaar = leeftijd - 1;
+    leeftijd_jaar = leeftijd_jaar - 1;
+    cout << leeftijd_jaar << endl;
   }
-
-}
-  else{
-  return 1;
+}else{
+  if (leeftijd_maand > 0){
+    cout << leeftijd_jaar << endl;
+}else{
+leeftijd_jaar = leeftijd_jaar - 1;
+cout << leeftijd_jaar << endl;
+  }
 }
 
 return 0;
+
 }
