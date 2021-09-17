1647cb40fe40d0d65725553fb71c1ec8d6ab378d
DMR-max <60442394+DMR-max@users.noreply.github.com>
2021-09-17 12:52:41 +0200

    nog testen of antwoorden kloppen

--- leeftijd.cc
+++ leeftijd.cc
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <ctime>
+#include <cstdlib>
+#include <cmath>
 using namespace std;
 
 int main ( ) {
+  srand (time(NULL)); //ranodmizen met behulp van huidige tijd
     int jaar;
     int maand;
     int dag;
@@ -11,6 +14,14 @@ int main ( ) {
     int leeftijd_maand;
     int leeftijd;
     int leeftijd_in_maand;
+    int a;
+    int b;
+    int c;
+    double discriminant;
+    int invoer_kwadraat;
+    int antwoord_kwadraat;
+    double echt_antwoord_kwadraat1;
+    double echt_antwoord_kwadraat2;
     tm s;
     time_t t;
 
@@ -86,6 +97,38 @@ int main ( ) {
             cout << leeftijd_jaar << endl; //je bent .. jaar oud
               }
             }
+            a = rand ( ) % 40;
+            b = rand ( ) % 50;
+            c = rand ( ) % 30;
+            cout << "los op: " << a << "x^2 + " << b << "x + " << c << endl; //formule
+            discriminant = (pow(b,2))-4*a*c;
+            cout << "voer in 0, 1 of 2 als aantal oplossingen" << endl;
+            cin >> invoer_kwadraat;
+            if (discriminant == 0){
+              antwoord_kwadraat = 1;
+            }else if (discriminant > 0){
+              antwoord_kwadraat = 2;
+            }else{
+              antwoord_kwadraat = 0;
+            }
+            if (antwoord_kwadraat == invoer_kwadraat){
+              cout << "Gefeliciteerd u heeft het correcte antwoord" << endl;
+            }else{
+              cout << "Helaas het antwoord klopt niet." << endl;
+            }
+            if (antwoord_kwadraat == 2){
+              echt_antwoord_kwadraat1 = (-b-sqrt(discriminant))/(2*a);
+              echt_antwoord_kwadraat2 = (-b+sqrt(discriminant))/(2*a);
+              cout << "De antwoorden zijn: x=" << echt_antwoord_kwadraat1 << " en x=" << echt_antwoord_kwadraat2  << endl;
+            }
+          else{ if (antwoord_kwadraat == 1){
+            echt_antwoord_kwadraat1 = (-b)/(2*a);
+            cout << "Het antwoord is: x=" << echt_antwoord_kwadraat1 << endl;
+                }
+
+          }
+
+
                 //Hier verder met code!
 
               }
