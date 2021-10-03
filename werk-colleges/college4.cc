93a0110cb3e746bfed76f8839b8da878a9cf1a67
DMR-max <60442394+DMR-max@users.noreply.github.com>
2021-10-03 10:10:56 +0200

    functies

--- werk-colleges/college4.cc
+++ werk-colleges/college4.cc
@@ -1,6 +1,56 @@
 #include <iostream>
 using namespace std;
-   int main ( ) {
+int u = 8;
+int v = 12;
+int antwoord;
+int macht;
+double nummer;
+int b, z, a, i;
+void infoblokje(){
+  cout << "makers: Sjouk Ketwaru (s3287297),"
+  << "Ryan Sleeuwaegen (s3122166)" << endl;
+  cout << "jaar van aankomst: 2021" << endl;
+  cout << "studierichting: Kunstmatige Intelligentie" << endl;
+  cout << "opgave 1, gemaakt op 23-09-2021" << endl;
+  cout << "Dit is een test om aangenomen te worden "
+  << "aan de universiteit." << endl;
+}
 
-     return 0;
-   }//main
+int telop (int x, int y){
+  antwoord = x + y;
+  return antwoord;
+}
+
+void telop2(int x, int y, int & z){
+  z = x + y;
+}
+
+//void telop3(int x1, int x2, int y1, int y2, int & z1, int & z2){
+  //z1 = (x1 * y2) + (y1 * x2);
+  //z2 = x2 * y2;
+//}
+
+int main ( ) {
+  infoblokje();
+
+  telop(11,12);
+  telop(u,v);
+  cout << telop(11,12) << endl;
+  u = telop(11,12);
+  cout << u << endl;
+
+  telop2(11, 12, z);
+  cout << z << endl << endl;
+
+  //telop3(1, 6, 1, 5);
+  //cout << z1 << "/" << z2 << endl;
+
+  cin >> macht;
+  cin >> nummer;
+  for (i = 0; i != macht;){
+    a = nummer * nummer;
+    i++;
+  }
+  cout << a << endl;
+  return 0;
+}//main
