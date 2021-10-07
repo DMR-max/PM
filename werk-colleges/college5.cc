848a3044c2817373f13dfc707bf6a1583bbadaa1
Sjouk Ketwaru <katwaru18@gmail.com>
2021-10-07 16:05:01 +0200

    college 5

--- werk-colleges/college5.cc
+++ werk-colleges/college5.cc
@@ -3,14 +3,27 @@
 using namespace std;
 
 
-  ifstream invoer ("invoer.txt"); // denk aan <fstream>
-  ofstream uitvoer ("uitvoer.txt");
+ifstream invoer ("invoer.txt", ios::in | ios::binary); // denk aan <fstream>
+ofstream uitvoer ("uitvoer.txt", ios::out | ios::binary);
 int main (){
+  int karakters;
+  karakters = 0;
   char kar = invoer.get ( );
   while ( ! invoer.eof ( ) ) {
-    uitvoer.put (kar); // of uitvoer << kar;
+    if ( '0' <= kar && kar <= '9' ){
+      cout << kar << endl;
+      int b = kar;
+      int a = b;
+      cout << a << endl;
+    }else{
+      uitvoer.put (kar); // of uitvoer << kar;
+    }
     kar = invoer.get ( );
+    karakters++;
+
   }//while
+  cout << karakters << endl;
   invoer.close ( );
   uitvoer.close ( );
+
 }
