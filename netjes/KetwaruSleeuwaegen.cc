c9cfb8151c323ae20f5a61707128f820a5319edc
RyanardoDaVinci <ryansleeuwaegen@gmail.com>
2021-10-07 14:16:13 +0200

    :D

---
+++ netjes/KetwaruSleeuwaegen.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+/*
+Auteurs: Sjouk Ketwaru en Ryan Sleeuwaegen
+naam bestand: KetwaruSleeuwaegen.cc
+*Beschrijving van t bestand*
+text editor: Atom, compiler: MinGW.
+Laatst aan gewerkt op: .
+ */
+
+ 
