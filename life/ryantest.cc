0512e67ba0a9b0f7552b087b9f468b5f3e2746b5
RyanardoDaVinci <ryansleeuwaegen@gmail.com>
2021-11-15 22:39:55 +0100

    doneeeeee

--- life/ryantest.cc
+++ life/ryantest.cc
@@ -7,7 +7,7 @@ Er is gekozen voor een eindige wereld van 1000 x 1000, dit om
 problemen te voorkomen. De gebruiker ziet een klein deel van deze
 wereld, de view. Via het menu kan de gebruiker uit meerde opties kiezen.
 
-Text editor: Atom (ubuntu), Compiler: G++.
+Text editor: Atom (ubuntu), Compiler: G++ versie: 9.3.0.
 Laatst aan gewerkt op: 15-11-2021.
 */
 
@@ -66,8 +66,8 @@ class life {
       breedte = 80; //Breedte van de view, kan worden aangepast
       cursorhoogte = hoogte / 2; //Verticale positie v.d. cursor
       cursorbreedte = breedte / 2; //Horizontale positive v.d. cursor
-
     }
+
     void drukaf ( );
     void heelschoon ( );
     void schoon ( );
@@ -105,10 +105,10 @@ class life {
     int verschil_hoogte = 25; //Nodig om view te wijzigen voor toggle
 };
 
-//Functie om je view te printen
-void life::drukaf ( ){
+//Print je view
+void life::drukaf ( ) {
   int i = 0, j = 0;
-
+  //for loop om in de juiste hoogte en breedte af te drukken
   for ( i = schuif; i < hoogte; i++ ) {
     for ( j = schuifbreedte; j < breedte; j++ ) {
       //Check voor world border (is altijd dood, en is een '=')
@@ -116,8 +116,8 @@ void life::drukaf ( ){
         dewereld[i][j] = false;
         cout << "=";
       //Cursor als de cel levend is
-      }else if (dewereld[i][j]) {
-        if (i == cursorhoogte && j == cursorbreedte){
+      }else if ( dewereld[i][j] ) {
+        if ( i == cursorhoogte && j == cursorbreedte ) {
           cout << "#";
         } else {
           cout << kar_levend;
@@ -125,7 +125,7 @@ void life::drukaf ( ){
       }
       //Cursor als de cel dood is
       else {
-        if (i == cursorhoogte && j == cursorbreedte){
+        if ( i == cursorhoogte && j == cursorbreedte ) {
           cout << "$";
         } else {
           cout << kar_dood;
@@ -137,43 +137,43 @@ void life::drukaf ( ){
   }//for i
 }//life::drukaf
 
-// Functie om inputs te filteren zodat het alleen getallen zijn
+// Input filteren naar alleen getallen
 void life::LeesGetal ( ) {
   char kar;
 
   kar = cin.get ( );
   // != 1 om het verschil tussen percentage en getal weer te geven
   // (percentage mag niet voorbij 100, getal niet voorbij 1000)
-  if (percentage != 1){
+  if ( percentage != 1 ) {
     // Zolang input een enter is opnieuw letter vragen
-    while (kar == '\n'){
-      kar = cin.get( );
+    while ( kar == '\n' ) {
+      kar = cin.get ( );
     }
     // Zorgen dat elk getal (behalve percentage) niet groter dan 1000 kan zijn
-    while (kar !='\n'){
-      if ('0' <= kar && kar <= '9' && getal < 1000) {
+    while (kar !='\n') {
+      if ( '0' <= kar && kar <= '9' && getal < 1000 ) {
         getal= 10 * getal + (kar - '0');
       }
-      kar = cin.get( );
+      kar = cin.get ( );
     }
-  }else{
+  } else {
     percentage = 0;
-    kar = cin.get( );
+    kar = cin.get ( );
     //Zorgen dat percentage niet groter dan 100 kan zijn
-    while (kar !='\n'){
-      if ('0' <= kar && kar <= '9' && percentage * 10 < 101) {
-        percentage = 10 * percentage + (kar - '0');
+    while ( kar !='\n' ) {
+      if ( '0' <= kar && kar <= '9' && percentage * 10 < 101 ) {
+        percentage = 10 * percentage + ( kar - '0' );
       }
-      kar = cin.get( );
+      kar = cin.get ( );
     }
-    if (percentage > 100 && percentage < 110) {
+    if ( percentage > 100 && percentage < 110 ) {
       percentage = percentage / 10;
     }
     cout << "Percentage: " << percentage << endl;
   }
 } //life::leesGetal
 
-//Functie om alle cellen in de wereld dood te maken
+//Maak alle cellen in de wereld dood
 void life::heelschoon ( ) {
   int i, j;
 
@@ -184,7 +184,7 @@ void life::heelschoon ( ) {
   } //for i
 } //life::heelschoon
 
-//Functie om alle cellen in de view dood te maken
+//Maak alle cellen in de view dood
 void life::schoon ( ) {
   int i, j;
 
@@ -195,28 +195,26 @@ void life::schoon ( ) {
   } //for i
 } //life::schoon
 
-//Functie om je view te verschuiven
+//Verschuif je view naar links, rechts, boven of beneden
 int life::verschuif ( ) {
-  int j, i = 0;
   char letterinvoer;
+  bool error = false;
 
   cout << endl << "Z = Omlaag, W = Omhoog, A = Links, D = Rechts " << endl
        << "Voer een letter in: ";
-  letterinvoer = cin.get( );
+  letterinvoer = cin.get ( );
   //Zolang input een enter is nieuwe input vragen
   while (letterinvoer == '\n') {
-    letterinvoer = cin.get( );
+    letterinvoer = cin.get ( );
   }
   //Switch voor naar links, rechts, boven en onder
-  switch (letterinvoer) {
+  switch ( letterinvoer ) {
     //Omlaag gaan
     case 'Z': case 'z':
-      if (hoogte + omlaag > MAX){
-        cout << "Dat lukte niet, de wereldrand wordt "
-             << "bereikt met deze verschuiving." << endl;
+      if (hoogte + omlaag >= MAX){
+        error = true;
         omlaag = 1;
       } else {
-        cout << omlaag << endl;
         schuif = schuif + omlaag;
         cursorhoogte = cursorhoogte + omlaag;
         hoogte = hoogte + omlaag;
@@ -224,12 +222,10 @@ int life::verschuif ( ) {
       break;
     //Omhoog gaan
     case 'W': case 'w':
-      if (schuif - omhoog < 0){ //BORDER DETECTIE TE SNEL AL BIJ 39 IPV 41!!
-        cout << "Dat lukte niet, de wereldrand wordt "
-             << "bereikt met deze verschuiving." << endl;
+      if ( schuif - omhoog <= 0 ) { //BORDER DETECTIE TE SNEL AL BIJ 39 IPV 41!!
+        error = true;
         omhoog = 1;
       } else {
-        cout << omhoog << endl;
         schuif = schuif - omhoog;
         cursorhoogte = cursorhoogte - omhoog;
         hoogte = hoogte - omhoog;
@@ -237,9 +233,8 @@ int life::verschuif ( ) {
       break;
     //Naar links gaan
     case 'A': case 'a':
-      if (schuifbreedte - links < 0){
-        cout << "Dat lukte niet, de wereldrand wordt "
-             << "bereikt met deze verschuiving." << endl;
+      if ( schuifbreedte - links <= 0 ) {
+        error = true;
         links = 1;
       } else {
         schuifbreedte = schuifbreedte - links;
@@ -249,9 +244,8 @@ int life::verschuif ( ) {
       break;
     //Naar rechts gaan
     case 'D': case 'd':
-      if (breedte + rechts > MAX){
-        cout << "Dat lukte niet, de wereldrand wordt "
-             << "bereikt met deze verschuiving." << endl;
+      if ( breedte + rechts >= MAX ) {
+        error = true;
         rechts = 1;
       } else {
         schuifbreedte = schuifbreedte + rechts;
@@ -260,39 +254,41 @@ int life::verschuif ( ) {
       }
       break;
   } //Switch
+  if ( error == true ) {
+    cout << "Dat lukte niet, de wereldrand wordt "
+         << "bereikt met deze verschuiving." << endl;
+  }
   return 1;
 } //life::verschuiving
 
-//Functie om de groote van elke verschuiving te bepalen
+//Bepaal met hoeveel cellen per keer je je view kan verschuiven
 int life::verschuivingsstap ( ) {
-  char omlaag_invoer, omhoog_invoer, links_invoer, rechts_invoer;
-
   //Aantal cellen omlaag gaan
   cout << endl << "Voer in hoeveel u omlaag wil gaan in: ";
-  LeesGetal();
+  LeesGetal ( );
   omlaag = getal;
   getal = 0;
   //Aantal cellen omhoog gaan
   cout << "Voer in hoeveel u omhoog wil gaan in: ";
-  LeesGetal();
+  LeesGetal ( );
   omhoog = getal;
   getal = 0;
   //Aantal cellen naar rechts gaan
   cout << "Voer in hoeveel u naar rechts wil gaan in: ";
-  LeesGetal();
+  LeesGetal ( );
   rechts = getal;
   getal = 0;
   //Aantal cellen naar links gaan
   cout << "Voer in hoeveel u naar links wil gaan in:  ";
-  LeesGetal();
+  LeesGetal ( );
   links = getal;
   getal = 0;
   //Naar de verschuif functie
-  verschuif( );
+  verschuif ( );
   return 1;
 } //life::verschuivingsstap
 
-//Functie om view met random cellen te vullen (wordt hier niet afgedrukt)
+//Vul de view met random cellen (wordt hier nog niet geprint)
 void life::randomizer ( ) {
   static int randomgetal = 42; //Seed
   int mogelijkheid;
@@ -320,56 +316,37 @@ void life::randomizer ( ) {
   } //for i
 } //life::randomizer
 
-//Functie om de cursor levend of dood te maken, en te verplaatsen
+//Swap de cursor tussen levend en dood, en verplaats de cursor
 void life::toggle ( ) {
   string regel;
   bool error = false;
   char n;
   int i, regellengte = 0;
 
+  cout << endl << "Z = Omlaag, W = Omhoog, A = Links, D = Rechts " << endl
+       << "Voer een letter in: ";
   cin >> n;
-  getline (cin, regel);
+  //Inlezen van string
+  getline ( cin, regel );
   regel = n + regel;
-  cout << regel << endl;
+  //Telt hoeveel karakters in een regel
   regellengte = regel.length ( );
-  cout << regellengte << endl;
+  //For loop om langs alle karakters individueel te gaan
+  for ( i = 0; i < regellengte; i++ ) {
+    //Switch voor wat het programma moet doen met ieder karakter
+    switch ( regel[i] ) {
 
-  for (i = 0; i < regellengte; i++){
-
-    //cout << "Dat lukte niet probeer de view aan te passen: " << endl;
-    if (cursorbreedte <= schuifbreedte){
-      schuifbreedte = schuifbreedte - verschil_breedte;
-      breedte = breedte - verschil_breedte;
-    }
-    if (cursorbreedte >= breedte){ // MAX VERSCHUIF TOCH NOG VERDER?!
-      schuifbreedte = schuifbreedte + verschil_breedte;
-      breedte = breedte + verschil_breedte;
-      if (schuifbreedte >= MAX - (verschil_breedte / 2)){
-        breedte = MAX;
-        schuifbreedte = MAX - verschil_breedte;
-      }
-    }
-    if (cursorhoogte <= hoogte){
-      schuif = schuif - verschil_hoogte;
-      hoogte = hoogte - verschil_hoogte;
-    }
-    if (cursorhoogte >= hoogte){
-      schuif = schuif + verschil_hoogte;
-      hoogte = hoogte + verschil_hoogte;
-    }
-    //menu();
-  //}else{
-  switch (regel[i]) {
-
-    case 'T': case 't':
-      if(dewereld[cursorhoogte][cursorbreedte]){
-        dewereld[cursorhoogte][cursorbreedte] = false;
-      }else{
-        dewereld[cursorhoogte][cursorbreedte] = true;
-      }
-      break;
+      case 'T': case 't':
+      //Als er een dode cel is levend maken anders dood maken
+        if( dewereld[cursorhoogte][cursorbreedte] ) {
+          dewereld[cursorhoogte][cursorbreedte] = false;
+        }else{
+          dewereld[cursorhoogte][cursorbreedte] = true;
+        }
+        break;
+      //WAZD voor het verplaatsen van de cursor
       case 'D': case 'd':
-        if (cursorbreedte + 1 >= MAX - 1){
+        if ( cursorbreedte + 1 >= MAX - 1 ) {
           error = true;
         }else{
           cursorbreedte = cursorbreedte + 1;
@@ -397,13 +374,43 @@ void life::toggle ( ) {
         }
         break;
     }
+    //View verplaatsen als de cursor rechts buiten het scherm is
+    if ( cursorbreedte >= breedte ) {
+      schuifbreedte = schuifbreedte + verschil_breedte;
+      breedte = breedte + verschil_breedte;
+      //Om de view net te houden bij de randen van het veld
+      if ( schuifbreedte >= MAX - ( verschil_breedte / 2 ) ) {
+        breedte = MAX;
+        schuifbreedte = MAX - verschil_breedte;
+      }
+    }
+    //View verplaatsen als de cursor links buiten het scherm is
+    if ( cursorbreedte <= schuifbreedte - 1 ) {
+      schuifbreedte = schuifbreedte - verschil_breedte;
+      breedte = breedte - verschil_breedte;
+      //Om de view net te houden bij de randen van het veld
+      if ( schuifbreedte <= 0 + ( verschil_breedte / 2 ) ) {
+        breedte = verschil_breedte;
+        schuifbreedte = 0;
+      }
+    }
+    //View verplaatsen als de cursor boven buiten het scherm is
+    if ( cursorhoogte <= hoogte - 1 ) {
+      schuif = schuif - verschil_hoogte;
+      hoogte = hoogte - verschil_hoogte;
+    }
+    //View verplaatsen als de cursor onder buiten het scherm is
+    if ( cursorhoogte >= hoogte ) {
+      schuif = schuif + verschil_hoogte;
+      hoogte = hoogte + verschil_hoogte;
+    }
   }
-  if (error == true){
+  if ( error == true ) {
     cout << "u gaat buiten de wereld" << endl;
   }
 }
 
-//Functie om een txt file in te lezen en in de wereld te zeten (zoals glidergun)
+//Lees txt file in en zet het in de wereld
 void life::file ( ) {
   string eigen_invoer;
   char kar;
@@ -414,20 +421,20 @@ void life::file ( ) {
   //Eerst de wereld leeg maken voordat het bestand wordt ingelezen
   heelschoon ( );
 
-  ifstream invoer (eigen_invoer, ios::in | ios::binary);
+  ifstream invoer ( eigen_invoer, ios::in | ios::binary );
   //Checken of het bestand bestaat
-  if (invoer.fail ( )){
+  if ( invoer.fail ( ) ) {
     cout << "Dit bestand bestaat niet." << endl;
     menu ( );
   }
   // Main while loop om karakters in te lezen
-  while (! invoer.eof ( )) {
+  while ( ! invoer.eof ( ) ) {
     kar = invoer.get ( );
-    if (kar == '\n'){
+    if ( kar == '\n' ) {
       i++;
       j = 0;
     }
-    if (kar == 'x'){
+    if ( kar == 'x' ) {
       dewereld[i][j] = true;
     }else{
     dewereld[i][j] = false;
@@ -436,7 +443,7 @@ void life::file ( ) {
   } //While
 } //life::file
 
-//Functie om 1 generatie te doen
+//Doet 1 generatie
 void life::een ( ) {
   int LevendeBuren;
   int l, r, b, o, i, j;
@@ -460,61 +467,61 @@ void life::een ( ) {
       //Aantal levende buren resetten
       LevendeBuren = 0;
       //Cel links van huidige cel checken
-      if (reserve[l][j]) {
+      if ( reserve[l][j] ) {
         LevendeBuren++;
       }
       //Cel rechts van huidige cel checken
-      if (reserve[r][j]) {
+      if ( reserve[r][j] ) {
         LevendeBuren++;
       }
       //Cel boven huidige cel checken
-      if (reserve[i][b]) {
+      if ( reserve[i][b] ) {
         LevendeBuren++;
       }
       //Cel onder huidige cel checken
-      if (reserve[i][o]) {
+      if ( reserve[i][o] ) {
         LevendeBuren++;
       }
       //Cel links boven van huidige cel checken
-      if (reserve[l][b]) {
+      if ( reserve[l][b] ) {
         LevendeBuren++;
       }
       //Cel rechts boven van huidige cel checken
-      if (reserve[r][b]) {
+      if ( reserve[r][b] ) {
         LevendeBuren++;
       }
       //cel links onder van huidige cel checken
-      if (reserve[l][o]) {
+      if ( reserve[l][o] ) {
         LevendeBuren++;
       }
       //Cel rechts onder van huidige cel checken
-      if (reserve[r][o]) {
+      if ( reserve[r][o] ) {
         LevendeBuren++;
       }
       //Alle cellen op dood zetten
       dewereld[i][j] = false;
       //Als de cel levend was en 2 of 3 buren heeft, dan blijft hij leven
-      if ((reserve[i][j] && LevendeBuren == 2)
-          || (reserve[i][j] && LevendeBuren == 3)) {
+      if ( ( reserve[i][j] && LevendeBuren == 2 )
+          || ( reserve[i][j] && LevendeBuren == 3 ) ) {
         dewereld[i][j] = true;
       //Als de cel dood was en 3 buren heeft, dan wordt hij levend
-      }else if (! reserve[i][j] && LevendeBuren == 3) {
+      }else if ( ! reserve[i][j] && LevendeBuren == 3 ) {
         dewereld[i][j] = true;
       }
     } //for j
   } //for i
 } //life::een
 
-//Functie om meerdere generaties te doen
+//Doet meerdere generaties
 void life::gaan ( ) {
   int a;
   int generatie = 0;
 
   //Aantal generaties vragen
   cout << endl << "Hoeveel generaties: ";
-  LeesGetal( );
+  LeesGetal ( );
   //For loop om alle generaties te printen
-  for (a = 1; a <= getal; a++) {
+  for ( a = 1; a <= getal; a++ ) {
     een ( );
     drukaf ( );
     generatie++;
@@ -525,38 +532,38 @@ void life::gaan ( ) {
   generatie = 0;
 }//life::gaan
 
-//Functie om percentage op te vragen
+//Vraag percentage op
 void life::Percentage ( ) {
   cout << endl << "Kies een percentage tussen 0 en 100: ";
   percentage = 1;
   LeesGetal ( );
 } //life::Percentage
 
-//Functie om je karakter voor levende en dode cellen te kiezen
+//Kies karakter voor levende en dode cellen
 void life::karakters ( ) {
 
   //Karakter voor levende cellen kiezen
   cout << "Kies een karakter voor levende cellen: ";
-  kar_levend = cin.get( );
+  kar_levend = cin.get ( );
   //Zolang input een enter is nieuwe input vragen
-  while (kar_levend == '\n') {
-    kar_levend = cin.get( );
+  while ( kar_levend == '\n' ) {
+    kar_levend = cin.get ( );
   }
   //Karakter voor dode cellen kiezen
   cout << "Kies een karakter voor dode cellen: ";
-  kar_dood = cin.get( );
+  kar_dood = cin.get ( );
   //Zolang input een enter is nieuwe input vragen
-  while (kar_dood == '\n') {
-    kar_dood = cin.get( );
+  while ( kar_dood == '\n' ) {
+    kar_dood = cin.get ( );
   }
 } //life::karakters
 
-//Functie om de grootte van je view te bepalen
+//Bepaal grootte van je view
 void life::view ( ) {
 
   //Hoogte van je view bepalen
   cout << endl << "Voer de hoogte van de view in: ";
-  LeesGetal();
+  LeesGetal ( );
   hoogte = getal;
   verschil_hoogte = getal;
   //Verticale positie cursor bepalen
@@ -564,7 +571,7 @@ void life::view ( ) {
   getal = 0;
   //Breedte van je view bepalen
   cout << "Voer de breedte van de view in: ";
-  LeesGetal();
+  LeesGetal ( );
   breedte = getal;
   verschil_breedte = getal;
   //Horizontale positie cursor bepalen
@@ -583,14 +590,14 @@ void life::parameters ( ) {
   cout << "Kies een letter: ";
 
   //Letter vragen
-  letter = cin.get( );
+  letter = cin.get ( );
   //Zolang input een enter is nieuwe input vragen
-  while (letter == '\n') {
-    letter = cin.get( );
-}
-  while (letter != 'T' && letter != 't'){
+  while ( letter == '\n' ) {
+    letter = cin.get ( );
+  }
+  while ( letter != 'T' && letter != 't' ) {
 
-    switch (letter) {
+    switch ( letter ) {
 
       case 'V': case 'v':
         verschuivingsstap ( );
@@ -617,10 +624,10 @@ void life::parameters ( ) {
         break;
 
       case 'S': case 's':
-        exit(1);
+        exit ( 1 );
     } // switch
     // Als de input geen optie is dit printen en opnieuw submenu weergeven
-    if (fout == true) {
+    if ( fout == true ) {
      cout << "Geen optie!" << endl << endl;
      parameters ( );
   }
@@ -633,7 +640,7 @@ void life::parameters ( ) {
   return;
 } //life::parameters
 
-//Functie voor hoofdmenu
+//Hoofdmenu
 void life::menu ( ) {
   char letter;
   bool fout; //Check of de input wel een optie is
@@ -642,7 +649,7 @@ void life::menu ( ) {
   cout << endl << "Cursor Coordinaten:" << "(" << cursorbreedte << ", "
   << cursorhoogte << ")" << endl;
   //Eerst de view printen, daarna opnieuw het menu weergeven
-  drukaf( );
+  drukaf ( );
 
   cout << "Menu: (S)toppen, (H)eelschoon, S(c)hoon, "
        << "(V)erschuif, (P)arameters, (R)andom, "
@@ -650,16 +657,16 @@ void life::menu ( ) {
   cout << "Kies een letter: ";
 
   //Letter vragen
-  letter = cin.get( );
+  letter = cin.get ( );
 
   //Zolang input een enter is nieuwe input vragen
-  while (letter == '\n') {
-    letter = cin.get( );
+  while ( letter == '\n' ) {
+    letter = cin.get ( );
   }
 
-  while (letter != 'S' && letter != 's'){
+  while ( letter != 'S' && letter != 's' ) {
 
-    switch (letter) {
+    switch ( letter ) {
 
       case 'H': case 'h':
         heelschoon ( );
@@ -715,7 +722,7 @@ void life::menu ( ) {
         break;
     } // switch
     // Als de input geen optie is dit printen en opnieuw hoofdmenu weergeven
-    if (fout == true) {
+    if ( fout == true ) {
       cout << "Geen optie!" << endl << endl;
       menu ( );
     }
@@ -725,12 +732,12 @@ void life::menu ( ) {
   } // while
   //Stoppen als input S of s is
   cout << "Stop" << endl;
-  exit(1);
+  exit ( 1 );
 } //life::menu
 
 //Main functie om de code te runnen, begint met de functie menu runnen
 int main ( ) {
   life M;
-  infoblokje( );
+  infoblokje ( );
   M.menu ( );
 } //main
