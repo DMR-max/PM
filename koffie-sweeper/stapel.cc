403004f8c2f5dc8b566167d261c384fd002e0e9e
DMR-max <60442394+DMR-max@users.noreply.github.com>
2021-12-13 19:25:58 +0100

    alles met een tweetje es ouder

--- koffie-sweeper/stapel.cc
+++ koffie-sweeper/stapel.cc
@@ -1,64 +1,167 @@
 #include <iostream>
+#include "stapel.h"
 using namespace std;
 
-class vakje { // een struct mag ook
-  public:
-    // constructor (een destructor hoeft misschien niet)
-    vakje ( ) { // beter niet "inline"
-      info = 0; volgende = nullptr; }// constructor vakje
-      int info;
-      vakje* volgende;
-};//vakje
-class stapel { // de stapel zelf
-  public:
-      stapel ( ) {
-        bovenste = nullptr; }// maak lege stapel
-        ~stapel ( ); // destructor
-        void zetopstapel (int); // push
-        void haalvanstapel (int&); // pop
-        bool isstapelleeg ( ) { // is stapel leeg?
-          //return ( ( bovenste == nullptr ) ? true : false )
-          if (bovenste == nullptr){
-            return 1;
-          }else{
-            return 0;
-          }
-
-        // of: if ( bovenste == nullptr ) ...
-
-        }//isstapelleeg
-
-  private: // het begin van de lijst is
-    vakje * bovenste; // de bovenkant van de stapel
-};//stapel
-
-void stapel::zetopstapel (int getal) { // push
-  vakje * temp = new vakje;
-  temp->info = getal;
+void stapel::zetopstapel (bordvakje * koffiebord) { // push
+  bordvakje * temp = new bordvakje;
+  temp->info = koffiebord;
   temp->volgende = bovenste;
   bovenste = temp;
-  cout << bovenste << endl;
-  cout << temp << endl;
+  cout << koffiebord -> geopend << endl;
+
+  /*bordvakje * temp = getal;
+  temp->info = getal;
+  temp->volgende = bovenste;
+  bovenste = temp; */
+
+  // Printen y-coords
+  //if (temp == nullptr && bovenste != nullptr) {
+    //cout << endl << h % 10 << '\t';
+    //h++;
+
+    //temp = bovenste;
+    //bovenste = temp -> buren[4];
+  //}
+//}
+  //printf("%d\n", getal);
+
 }//stapel::zetopstapel
-void stapel::haalvanstapel (int & getal) { // pop
-  vakje * temp = bovenste;
-  getal = bovenste->info;
+void stapel::haalvanstapel (bordvakje * koffiebord, int breedte, int hoogte) { // pop
+  bordvakje* temp = bovenste;
+  koffiebord = bovenste->info;
   bovenste = bovenste->volgende;
-  delete temp;
-}//stapel::haalvanstapel
+  int teller = 0;
+  while (breedte*hoogte > teller){
+    cout << bovenste << endl;
+    teller ++;
+  }
+  //delete temp;
+}
+
+//stapel::haalvanstapel
+
 stapel::~stapel ( ) {
   int getal;
-  while ( ! isstapelleeg ( ) ){
-    haalvanstapel (getal);
-  }
+  //while ( ! isstapelleeg ( ) ){
+    //haalvanstapel (getal);
+  //}
 }//stapel::~stapels
-int main ( ) {
-  stapel S;
+
+/*int main ( ) {
+  //stapel S;
   int getal;
   cout << "geef getal op:" << endl;
   cin >> getal;
-  S.zetopstapel(getal);
-  S.haalvanstapel(bovenste);
-  cout << bovenste << endl;
+  //cout << buren[2] << endl;
+  //S.zetopstapel(getal);
+  //S.haalvanstapel(getal);
+  cout << getal << endl;
   return 0;
-};
+}; */
+
+/*bordvakje * temp = bovenste_kar;
+char karakter;
+int teller;
+int h = 1;
+int hoogte_meter = 1;
+/*cout << bovenste_kar << endl;
+karakter = bovenste_kar ->info_kar;
+bovenste_kar = bovenste_kar->volgende; */
+/*teller = 0;
+for (int b = 0; b < breedte; b++) {
+  if (b == 0){
+    cout << '\t' << b % 10 << ' ';
+  }else{
+    cout << b % 10 << ' ';
+  }
+}
+cout << endl << endl << "0" << '\t';
+  while (breedte*hoogte > teller){
+    karakter = bovenste_kar->info_kar;
+    bovenste_kar = bovenste_kar->volgende;
+    cout << karakter << " ";
+    if (h == breedte && hoogte_meter != hoogte){
+      cout << endl << hoogte_meter % 10 << '\t';
+      h = 0;
+      hoogte_meter++;
+      /*if (hoogte_meter == hoogte - 1){
+        h = 1;
+      }else{
+        h = 1;
+      }*/
+    /*}
+    teller ++;
+    h++;
+  }
+
+/*  while (bovenste_kar != nullptr) {
+
+
+
+    if (!bovenste_kar -> geopend) {
+      if (!bovenste_kar -> gemarkeerd) {
+        //if (bovenste_kar -> koffie && zetten ==1) {
+          //aantal_koffie++;
+        //}
+       cout << ". ";
+      }
+      else {
+        cout << "# ";
+      }
+    }
+    else if (bovenste_kar -> koffie) {
+      cout << "K ";
+    }
+    else {
+      cout << bovenste_kar -> aantal << " ";
+    }
+
+    bovenste_kar = bovenste_kar -> buren[2];
+
+    // Printen y-coords
+    if (bovenste_kar == nullptr) {
+      cout << endl << h % 10 << '\t';
+      h++;
+
+    }*/
+
+
+
+/*for (int i = 0; i < 1; i++){
+//while (bovenste != nullptr){
+
+  if (!temp -> geopend) {
+    if (!temp -> gemarkeerd) {
+      //if (temp -> koffie && zetten ==1) {
+        //aantal_koffie++;
+      //}
+      cout << ". ";
+    }
+    else {
+      cout << "# ";
+    }
+  }
+  else if (temp -> koffie) {
+    cout << "K ";
+  }
+  else {
+    cout << temp -> aantal << " ";
+  }
+
+  temp = temp -> buren[2];
+  //getal = bovenste->info;
+  bovenste = bovenste->volgende;
+}*/
+
+/*int teller;
+bordvakje * temp_kar = bovenste_kar;
+karakter = bovenste_kar->info_kar;
+bovenste_kar = bovenste_kar->volgende;
+teller = 1;
+
+while (breedte*hoogte > teller){
+  karakter = bovenste_kar->info_kar;
+  bovenste_kar = bovenste_kar->volgende;
+  cout << karakter << endl;
+  teller ++;
+}*/
---
+++ koffie-sweeper/stapel.h
@@ -0,0 +1,40 @@
+#include <iostream>
+#include "koffiebord.h"
+using namespace std;
+
+class vakje { // een struct mag ook
+  public:
+    // constructor (een destructor hoeft misschien niet)
+    vakje ( ) { // beter niet "inline"
+      info = 0; volgende = nullptr; }// constructor vakje
+      bordvakje * info;
+      koffiebord * info_kar;
+      bordvakje* volgende;
+};//vakje
+class stapel { // de stapel zelf
+  public:
+      stapel ( ) {
+        bovenste = nullptr;
+        bovenste_kar = nullptr;
+       }// maak lege stapel
+        ~stapel ( ); // destructor
+        void zetopstapel (bordvakje * koffiebord); // push
+        void haalvanstapel (bordvakje * koffiebord, int breedte, int hoogte); // pop
+        bool isstapelleeg ( ) { // is stapel leeg?
+          //return ( ( bovenste == nullptr ) ? true : false )
+          if (bovenste == nullptr){
+            return 1;
+          }else{
+            return 0;
+          }
+
+        //stapel * pok;
+
+        // of: if ( bovenste == nullptr ) ...
+
+        }//isstapelleeg
+
+  private: // het begin van de lijst is
+    bordvakje * bovenste; // de bovenkant van de stapel
+    bordvakje * bovenste_kar;
+};//stapel
